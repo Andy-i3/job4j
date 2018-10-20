efabd42b2b57d09087fa98f3cb22ced70a4b2530
interwebandy <interwebandy@gmail.com>
2018-10-20 22:27:30 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,26 @@
 package ru.job4j.calculate; 
 
+/*
+* Returns "Hello world" 
+* @param no param
+* @return "Hello world"
+* @author Golubkov Andrey 
+* @version $Id$
+* @since 0.1
+*
+*/
+
+
 public class Calculate {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        
+ /*
+*  
+* @return "Hello world"
+*
+*/
+		
+		System.out.println("Hello world");
        
     }
 }
