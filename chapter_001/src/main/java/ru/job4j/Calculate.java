61dd870ebcfbe6ae807bf95cd5a0cd2d26a41637
Andy-i3 <interwebandy@gmail.com>
2018-10-30 17:26:12 +0300

    renamed:    chapter_001/src/main/java/ru/job4j/calculate/Calculate.java -> chapter_001/src/main/java/ru/job4j/Calculate.java 	renamed:    chapter_001/src/main/java/ru/job4j/calculate/package-info.java -> chapter_001/src/main/java/ru/job4j/package-info.java 	renamed:    chapter_001/test/java/ru/job4j/calculate/CalculateTest.java -> chapter_001/src/test/java/ru/job4j/CalculateTest.java 	renamed:    chapter_001/test/java/ru/job4j/calculate/package-info.java -> chapter_001/src/test/java/ru/job4j/package-info.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,33 +1,23 @@
 package ru.job4j.calculate; 
-
-
 /*
 * Calculate.
 * @author Golubkov Andrey 
 * @version $Id$
 * @since 0.1
-*
 */
-
 public class Calculate {
-    public static void main(String[] args) {
-        
- /*
-*  
+/*
 * @return "Hello world"
-*
 */
-		
-		System.out.println("Hello world");
-       
+    public static void main(String[] args) {
+    	System.out.println("Hello world");
     }
-
-	/**
+/**
 * Method echo.
 * @param name Your name.
 * @return Echo plus your name.
 */
-public String echo(String name) {
-    return "Echo, echo, echo : " + name;
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 }
 }
