ddc8dba3631905c3c2f1994b65f9af7eaab5724c
Andy-i3 <interwebandy@gmail.com>
2018-11-06 14:19:18 +0300

    TEST

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,22 +1,22 @@
-package ru.job4j.calculate; 
+package ru.job4j.calculate;
 /*
-* Calculate.
-* @author Golubkov Andrey 
-* @version $Id$
-* @since 0.1
-*/
+ * Calculate.
+ * @author Golubkov Andrey
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
 
 
-    public static void main(String[] args) {
-    	System.out.println("Hello world");
-    }
-/**
-* Method echo.
-* @param name Your name.
-* @return Echo plus your name.
-*/
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
-}
+	}
 }
