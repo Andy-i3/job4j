b42524d7d2e98ffdf0bf1e5c71f5c36a235a55f5
Andy-i3 <interwebandy@gmail.com>
2018-11-12 22:59:31 +0300

    3.2. Конвертер валюты. [#41583]

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -15,4 +15,31 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void whensubtract() {
+        Calculator calc = new Calculator();
+        calc.subtract(1D, 1D);
+        double result = calc.getResult();
+        double expected = 0D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whendiv() {
+        Calculator calc = new Calculator();
+        calc.div(1D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void multiple() {
+        Calculator calc = new Calculator();
+        calc.multiple(1D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
 }
