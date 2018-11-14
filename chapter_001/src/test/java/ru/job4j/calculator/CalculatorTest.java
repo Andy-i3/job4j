2615ede4f98cf0bd6b80ac8a60c132264580bc63
Andy-i3 <interwebandy@gmail.com>
2018-11-14 17:42:52 +0300

    3.4. Расстояние между точками в системе координат [#188]

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -9,10 +9,10 @@ public class CalculatorTest {
     @Test
     public void whenAddOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
-        calc.add( 1D, 1D );
+        calc.add(1D, 1D);
         double result = calc.getResult();
         double expected = 2D;
-        assertThat( result, is( expected ) );
+        assertThat(result, is( expected ));
     }
 
     @Test
