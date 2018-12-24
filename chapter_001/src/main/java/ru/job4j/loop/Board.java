71d6e9596bdabc67f67729aca84f7a3c9824ec1d
Andy-i3 <interwebandy@gmail.com>
2018-12-24 14:43:17 +0300

    5.3. Построить шахматную доску в псевдографике. [#13559]

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -4,16 +4,18 @@ public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        for (int y=0;y<height;y++) {
-            for (int x=y%2;x<width;x++) {
-                if (x%2 == 0) {
-                    screen.append( "X" );
-                   } else {
-                    screen.append( " " );
+        for (int y = 0; y < height; y++) {
+            for (int x = y % 2; x < width; x++) {
+                if (x % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
                 }
             }
-            if(y%2 == 1) screen.append( " " );
-            screen.append( ln );
+            if (y % 2 == 1) {
+                screen.append(" ");
+            }
+            screen.append(ln);
         }
         return screen.toString();
     }
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -15,7 +15,7 @@ public class BoardTest {
     @Test
     public void when3x3() {
         Board board = new Board();
-        String rsl = board.paint(3,3);
+        String rsl = board.paint(3, 3);
         String ln = System.lineSeparator();
         assertThat(rsl, is(
                 String.format("X X%s X %sX X%s", ln, ln, ln)
