edcb4f4170d6c9a48c2cc83fd67663418ef089a8
Andy-i3 <interwebandy@gmail.com>
2018-12-25 17:27:13 +0300

    5.3. Построить шахматную доску в псевдографике. [#13559]

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -5,17 +5,14 @@ public class Board {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
         for (int y = 0; y < height; y++) {
-            for (int x = y % 2; x < width; x++) {
-                if (x % 2 == 0) {
-                    screen.append("X");
+            for (int x = 0; x < width; x++) {
+                if ((x + y) % 2 == 0) {
+                    screen.append( "X" );
                 } else {
-                    screen.append(" ");
+                    screen.append( " " );
                 }
             }
-            if (y % 2 == 1) {
-                screen.append(" ");
-            }
-            screen.append(ln);
+            screen.append( ln );
         }
         return screen.toString();
     }
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -2,11 +2,11 @@ package ru.job4j.loop;
 
 
 import org.junit.Test;
-        import static org.hamcrest.core.Is.is;
-        import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
- *
  * @author
  * @version $Id$
  * @since 0.1
@@ -15,10 +15,20 @@ public class BoardTest {
     @Test
     public void when3x3() {
         Board board = new Board();
-        String rsl = board.paint(3, 3);
+        String rsl = board.paint( 3, 3 );
         String ln = System.lineSeparator();
-        assertThat(rsl, is(
-                String.format("X X%s X %sX X%s", ln, ln, ln)
+        assertThat( rsl, is(
+                String.format( "X X%s X %sX X%s", ln, ln, ln )
+                )
+        );
+    }
+
+    public void when4x5() {
+        Board board = new Board();
+        String rsl = board.paint( 4, 5 );
+        String ln = System.lineSeparator();
+        assertThat( rsl, is(
+                String.format( "X X %s X X%sX X %s X X%sX X %s", ln, ln, ln )
                 )
         );
     }
