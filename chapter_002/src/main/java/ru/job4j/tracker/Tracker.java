d813b22f2b3bb7354bfbd6df19d4a97c018acf89
Andrey <interwebandy@gmail.com>
2019-04-14 23:13:24 +0300

    2. Реализовать класс Tracker [#396]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,10 +1,15 @@
 package ru.job4j.tracker;
 
+import java.util.Random;
+
 /**
  * @version $Id$
  * @since 0.1
  */
 public class Tracker {
+
+    private static final Random RN = new Random();
+
     /**
      * Массив для хранение заявок.
      */
@@ -34,6 +39,41 @@ public class Tracker {
      */
     private String generateId() {
         //Реализовать метод генерации.
-        return null;
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
+    }
+
+    public void replace(String id, Item item) {
+    }
+
+    public Item findById(String id) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+    // public delete(String id) {
+
+    // Item[findById(id)].remove;
+
+    //   }
+
+    //  public Item[] findAll() {
+//
+    //  }
+
+    public Item findByName(String key) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
     }
 }
