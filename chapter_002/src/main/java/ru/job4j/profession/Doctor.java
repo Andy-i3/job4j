1b2afdbad822c878061d8bc2da19634115fd413b
Andrey <interwebandy@gmail.com>
2019-07-15 00:16:58 +0300

    1. Реализация профессий в коде [#6837]

--- chapter_002/src/main/java/ru/job4j/profession/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/profession/Doctor.java
@@ -1,18 +1,18 @@
 package ru.job4j.profession;
 
+import java.awt.*;
+
 public class Doctor extends Profession {
 
-    public class diagnose heal(
-    Pacient pacient)
+    public Diagnose heal(Pacient pacient) {
 
-    {
-
+        return null;
     }
-
 
     private static class Pacient {
         String namePacient;
     }
 
-
+    private class Diagnose {
+    }
 }
