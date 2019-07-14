2ebbed1bab37a0077f7c22a71f9ef773b42237be
Andrey <interwebandy@gmail.com>
2019-07-14 23:36:22 +0300

    1. Реализация профессий в коде [#6837] 1

--- chapter_002/src/main/java/ru/job4j/profession/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/profession/Doctor.java
@@ -2,5 +2,14 @@ package ru.job4j.profession;
 
 public class Doctor extends Profession {
 
+    public diagnose heal(Pacient pacient) {
+
+    }
+
+
+    private static class Pacient {
+        String namePacient;
+    }
+
 
 }
--- chapter_002/src/main/java/ru/job4j/profession/Profession.java
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -9,39 +9,45 @@ public class Profession {
     private String education;
     private Date birthday;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setEducation(String education) {
-        this.education = education;
-    }
-
-    public void setName(Date birthday) {
-        this.birthday = birthday;
-    }
-
     public String getName() {
+
         return this.name;
     }
 
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
     public String getSurname() {
+
         return this.surname;
     }
 
+    public void setSurname(String surname) {
+
+        this.surname = surname;
+    }
+
     public String getEducation() {
+
         return this.education;
     }
 
+    public void setEducation(String education) {
+
+        this.education = education;
+    }
+
     public Date getBirthday() {
+
         return this.birthday;
     }
 
+    public void setBirthday(Date birthday) {
 
+        this.birthday = birthday;
+    }
 }
 
 
