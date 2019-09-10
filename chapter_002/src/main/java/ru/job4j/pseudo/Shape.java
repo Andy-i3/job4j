9de9d7cc84d49f3e8064939e06655ea3a510d213
Голубков Андрей Александрович <golubkovaa@rushydro.ru>
2019-09-10 17:51:52 +0300

    Используя шаблон проектирования - стратегию [#786]

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Shape.java
@@ -0,0 +1,9 @@
+package ru.job4j.pseudo;
+
+public interface Shape {
+
+    String draw();
+
+}
+
+
---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -0,0 +1,8 @@
+package ru.job4j.pseudo;
+
+public class Triangle implements Shape{
+    @Override
+    public String draw() {
+        return null;
+    }
+}
