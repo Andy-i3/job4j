593da19f1478cb95fe57730f0ec2cfad80d65263
Голубков Андрей Александрович <golubkovaa@rushydro.ru>
2020-02-03 11:29:44 +0300

    0.4. Пользовательские исключения. (1)

--- chapter_002/src/main/java/ru/job4j/ex/FindEl.java
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -1,21 +1,28 @@
 package ru.job4j.ex;
 
+import java.util.Arrays;
+
 public class FindEl {
 
     public static void main(String[] args) {
-        String[] a = {"123", "234", "234"};
-        String b = "567";
+        String[] a = {"123", "234", "567"};
+        String b = "5674";
         try {
-            indexOf( a, b );
+            System.out.println(indexOf( a, b ));
         } catch (ElementNotFoundException e) {
-            System.out.println( e.getStackTrace() );
+            System.out.println( Arrays.toString( e.getStackTrace() ) );
         }
     }
 
     public static int indexOf(String[] value, String key) throws ElementNotFoundException{
         int rsl = -1;
+        int index = 0;
         for (String c : value) {
-            if (c == key) rsl=0;
+            if (c.equals( key )) {
+                rsl = index;
+                break;
+            }
+            ++index;
         }
         if (rsl == -1) throw new ElementNotFoundException("Error 123");
         return rsl;
