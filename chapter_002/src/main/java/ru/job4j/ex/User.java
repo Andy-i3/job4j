0628b03ec7c790477c7a4a4b3ca10c84545cb1d3
Голубков Андрей Александрович <golubkovaa@rushydro.ru>
2020-03-03 17:48:23 +0300

    0.5. Иерархия исключений и множественный catch. (3)

--- chapter_002/src/main/java/ru/job4j/ex/User.java
+++ chapter_002/src/main/java/ru/job4j/ex/User.java
@@ -3,18 +3,25 @@ package ru.job4j.ex;
 public class User {
 
     private String username;
-    private boolean valid;
+    private boolean valid = true;
 
     public User(String username, boolean valid) {
         this.username = username;
         this.valid = valid;
+
     }
+
 
     public String getUsername() {
         return username;
     }
 
     public boolean isValid() {
+
         return valid;
     }
 }
+
+//  if (getUsername().length()<3) {
+//        this.valid = false;
+//        }
