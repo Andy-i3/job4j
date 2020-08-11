1c06a65005a38a95ce0bb369cf9d83c216e842c8
Голубков Андрей Александрович <golubkovaa@rushydro.ru>
2020-08-11 17:58:52 +0300

    1.7. Состояние объекта. [#301658]

--- chapter_001/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_001/src/main/java/ru/job4j/oop/Battery.java
@@ -11,16 +11,16 @@ public class Battery {
 
         Battery first = new Battery(10);
         Battery second = new Battery(15);
-        first.exchange(second);
         System.out.println("First battery = " + first.load + " Second battry = "  + second.load);
         first.exchange(second);
         System.out.println("First battery = " + first.load + " Second battry = "  + second.load);
-    }
+            }
 
     public void exchange(Battery another) {
 
-            this.load = this.load - 1;
-            another.load = another.load + 1;
+            another.load = another.load + this.load;
+            this.load = 0;
+
 
         }
 }
