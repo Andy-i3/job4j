1b7303a9699808ccbffbebda8eb723f0c381c781
Голубков Андрей Александрович <golubkovaa@rushydro.ru>
2020-08-19 09:44:14 +0300

    2. Перегрузка конструктора. [#301664]

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -35,36 +35,45 @@ public class Item {
     public Item() {
 
     }
-    public Item(String id) {
 
+    public Item(String id) {
+        this.id = id;
     }
 
     public Item(String name, String id) {
-
+        this.name = name;
+        this.id = id;
     }
 
 
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDecs() {
         return decs;
     }
+
     public void setDecs(String decs) {
         this.decs = decs;
     }
+
     public long getTime() {
         return time;
     }
+
     public void setTime(long time) {
         this.time = time;
     }
