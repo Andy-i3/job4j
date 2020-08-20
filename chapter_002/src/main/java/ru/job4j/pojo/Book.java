e917ca83be5cdc8cd53ad714fcd6332b6bd87adf
Голубков Андрей Александрович <golubkovaa@rushydro.ru>
2020-08-20 12:34:34 +0300

    3. Массивы и модели. [#301671]

--- chapter_002/src/main/java/ru/job4j/pojo/Book.java
+++ chapter_002/src/main/java/ru/job4j/pojo/Book.java
@@ -5,6 +5,11 @@ public class Book {
     private String name;
     private int page;
 
+    public Book(String name, int page) {
+        this.name = name;
+        this.page = page;
+    }
+
     public String getName() {
         return name;
     }
--- chapter_002/src/main/java/ru/job4j/pojo/Library.java
+++ chapter_002/src/main/java/ru/job4j/pojo/Library.java
@@ -5,27 +5,11 @@ import java.awt.print.Printable;
 public class Library {
     public static void main(String[] args) {
 
-        Book book1 = new Book();
-        Book book2 = new Book();
-        Book book3 = new Book();
-        Book book4 = new Book();
-
-        book1.setName("One");
-        book2.setName("Two");
-        book3.setName("Three");
-        book4.setName("Clean Code");
-
-        book1.setPage(10);
-        book2.setPage(4);
-        book3.setPage(54);
-        book4.setPage(1);
-
         Book[] books = new Book[4];
-
-        books[0] = book1;
-        books[1] = book2;
-        books[2] = book3;
-        books[3] = book4;
+        books[0] = new Book("One",10);
+        books[1] = new Book("Two",15);
+        books[2] = new Book("Three",54);
+        books[3] = new Book("Clean Code",1);
 
         print(books);
 
