3a3455d4b5a7f71b066ba5cefb90525a4bea214a
Andrew <interwebandy@gmail.com>
2020-10-27 11:58:31 +0300

    5.1. Тестирование. Подготовка данных. [#301618]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -22,7 +22,7 @@ public class StartUI {
         }
     }
 
-    public static void editItem(Input input, Tracker tracker) {
+    public static void replaceItem(Input input, Tracker tracker) {
 
         System.out.println("=== Edit the Item ====");
         int id = Integer.parseInt(input.askStr("Enter ID: "));
@@ -83,7 +83,7 @@ public class StartUI {
             } else if (select == 1) {
                 StartUI.findAll(tracker);
             } else if (select == 2) {
-                StartUI.editItem(input, tracker);
+                StartUI.replaceItem(input, tracker);
             } else if (select == 3) {
                 StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
