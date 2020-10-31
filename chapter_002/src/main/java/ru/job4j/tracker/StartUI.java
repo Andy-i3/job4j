71c198f73f0ed8c651ee648020104473d025dbd3
Andrew <interwebandy@gmail.com>
2020-10-31 22:25:05 +0300

    9.1 Зависимость от System.out [#301607]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,28 +3,35 @@ package ru.job4j.tracker;
 
 public class StartUI {
 
-        public void init(Input input, Tracker tracker, UserAction[] actions) {
-            boolean run = true;
-            while (run) {
-                this.showMenu(actions);
-                int select = input.askInt("Select: ");
-                UserAction action = actions[select];
-                run = action.execute(input, tracker);
-            }
-        }
+    private final Output out;
 
-        private void showMenu(UserAction[] actions) {
-            System.out.println("Menu.");
-            for (int index = 0; index < actions.length; index++) {
-                System.out.println(index + ". " + actions[index].name());
-            }
+    public StartUI(Output out) {
+        this.out = out;
+    }
+
+    public void init(Input input, Tracker tracker, UserAction[] actions) {
+        boolean run = true;
+        while (run) {
+            this.showMenu(actions);
+            int select = input.askInt("Select: ");
+            UserAction action = actions[select];
+            run = action.execute(input, tracker);
         }
+    }
+
+    private void showMenu(UserAction[] actions) {
+        out.println("Menu.");
+        for (int index = 0; index < actions.length; index++) {
+            out.println(index + ". " + actions[index].name());
+        }
+    }
 
         public static void main(String[] args) {
+            Output output = new ConsoleOutput();
             Input input = new ConsoleInput();
             Tracker tracker = new Tracker();
             UserAction[] actions = {
-                    new CreateAction(),
+                    new CreateAction(output),
                     new FindAllAction(),
                     new ReplaceAction(),
                     new DeleteAction(),
@@ -32,6 +39,6 @@ public class StartUI {
                     new FindItemNameAction(),
                     new ExitAction()
             };
-            new StartUI().init(input, tracker, actions);
+            new StartUI(output).init(input, tracker, actions);
         }
     }
