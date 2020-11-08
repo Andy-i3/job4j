1ccabcdaf92f510ce4caa8e1fb4c9f496f5afd5c
Andrew <interwebandy@gmail.com>
2020-11-08 17:56:11 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#301628]

--- chapter_002/src/main/java/ru/job4j/ex/Fact.java
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -3,7 +3,7 @@ package ru.job4j.ex;
 public class Fact {
     public static void main(String[] args) {
         Fact h = new Fact();
-        System.out.println(h.calc(-20));
+        System.out.println(h.calc(5));
     }
 
     public int calc(int n) {
@@ -11,8 +11,8 @@ public class Fact {
             throw new IllegalArgumentException("Start should be less then finish.");
         }
         int rsl = 1;
-        for (int index = 1; index != n; index++) {
-            rsl += index;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
         }
         return rsl;
     }
