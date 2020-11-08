3bab6d9c9c5b5cb9367735866916c24850db24ec
Andrew <interwebandy@gmail.com>
2020-11-08 20:56:31 +0300

    0.3. Тестирование исключений с junit [#301629]

--- chapter_002/src/test/java/ru/job4j/ex/FactTest.java
+++ chapter_002/src/test/java/ru/job4j/ex/FactTest.java
@@ -2,13 +2,11 @@ package ru.job4j.ex;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class FactTest {
 
-//    @Test (expected = IllegalArgumentException.class)
-//    public void calc() {
-//        Fact d = new Fact();
-//        d.calc(10);
-//    }
+    @Test (expected = IllegalArgumentException.class)
+    public void calc() {
+        Fact d = new Fact();
+        d.calc(-10);
+    }
 }
