e06a6641bda570c6bc8010d680b4edc9ac800b55
Andrew <interwebandy@gmail.com>
2021-06-02 21:14:15 +0300

    5.7. Ипотека [#156315 #162319]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        double credit_end_year;
+        while (amount>0) {
+            year++;
+            credit_end_year = amount + amount * (percent/100);
+            amount = credit_end_year - salary;
+        }
+        return year;
+    }
+
+}
